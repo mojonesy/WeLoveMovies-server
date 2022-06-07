1432dac2ca23570baa97c981118fed83cd293161
Molly <mojonesycreative@gmail.com>
2022-06-07 17:32:42 -0400

    implemented read movie by id

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,7 +1,9 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/:movieId").get(controller.read).all(methodNotAllowed);
+
+router.route("/").get(controller.list).get(controller.movieIsShowing).all(methodNotAllowed);
 
 module.exports = router;
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -4,4 +4,19 @@ function list() {
     return knex("movies").select("*");
 }
 
-module.exports = { list, }
+function moviesShowing() {
+    return knex("movies as m")
+        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+        .select("m.*")
+        .where({"mt.is_showing": true });
+}
+
+function read(movieId) {
+    return knex("movies").select("*").where({ "movie_id": movieId }).first();
+}
+
+module.exports = { 
+    list, 
+    moviesShowing,
+    read,
+}
