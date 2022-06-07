8ad742ad68deba6b7e110eddf27fee7e19b0d062
Molly <mojonesycreative@gmail.com>
2022-06-07 11:26:58 -0400

    added 404 500 error handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
 // TODO: Set routes //
+
+
+// Error handlers //
+app.use(notFound);
+app.use(errorHandler);
+
 
 module.exports = app;
