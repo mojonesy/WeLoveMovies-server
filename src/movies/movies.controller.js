dd4df8ae2fff35bb509743d201db3f94c57bd4da
Molly <mojonesycreative@gmail.com>
2022-06-07 12:09:49 -0400

    implemented GET / to list movies

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,13 @@
+const service = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+async function list(req, res) {
+    const data = await service.list();
+    res.json({ data });
+}
+
+
+module.exports = {
+    list: asyncErrorBoundary(list),
+}
--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("./movies.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
