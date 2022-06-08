9fe3bad13c6c3dad56e221d582b2d040b8130161
Molly <mojonesycreative@gmail.com>
2022-06-08 12:14:40 -0400

    added /reviews route

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,13 +1,15 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-// TODO: implement the below route //
-router.route("/:movieId/reviews").all(methodNotAllowed);
+
+
+router.route("/:movieId/reviews").get(controller.reviewsByMovie).all(methodNotAllowed);
 
 router.route("/:movieId/theaters").get(controller.theatersWithMovie).all(methodNotAllowed);
 
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
-
+// Fix movieIsShowing //
 router.route("/").get(controller.list).get(controller.movieIsShowing).all(methodNotAllowed);
+
 
 module.exports = router;
