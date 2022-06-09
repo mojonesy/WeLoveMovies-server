191354c85cbf8b4e5e490ddd04cc0c71d8457578
Molly <mojonesycreative@gmail.com>
2022-06-09 13:19:02 -0400

    PUT request functional, added hasData validation

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -2,7 +2,21 @@ const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 
-const VALID_PROPERTIES = ["score", "content"];
+function hasData(req, res, next) {
+    if (req.body.data) {
+        return next();
+    }
+    next({status: 400, message: "body must have data property"});
+}
+
+const VALID_PROPERTIES = [
+    "review_id",
+    "content",
+    "score",
+    "critic_id",
+    "movie_id"
+];
+
 function hasOnlyValidProperties(req, res, next) {
   const { data = {} } = req.body;
   const invalidFields = Object.keys(data).filter(
@@ -24,7 +38,7 @@ function hasProperties(...properties) {
       try {
         properties.forEach((property) => {
           if(!data[property]) {
-            const error = new Error(`A ${property} is required.`)
+            const error = new Error(`A ${property} property is required.`)
             error.status = 400;
             throw error;
           }
@@ -45,14 +59,15 @@ async function reviewExists(req, res, next) {
     next({ status: 404, message: "Review cannot be found."});
 }
 
-
 async function update(req, res) {
     const updatedReview = {
         ...req.body.data,
         review_id: res.locals.review.review_id,
     };
-    const data = await service.update(updatedReview);
-    res.json({ data });
+
+    await service.update(updatedReview);
+    const addedCritics = await service.addCritics(res.locals.review.review_id);
+    res.json({ data: addedCritics });
 }
 
 async function destroy(req, res) {
@@ -65,8 +80,9 @@ async function destroy(req, res) {
 module.exports = {
     update: [
       asyncErrorBoundary(reviewExists), 
-    //   hasOnlyValidProperties, 
-    //   hasProperties("score", "content"),
+      hasData,
+      hasOnlyValidProperties, 
+      hasProperties("content"),
       asyncErrorBoundary(update)
     ],
     delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
