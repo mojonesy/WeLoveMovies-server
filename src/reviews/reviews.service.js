a9a0889360be0d2d4a21d397d6a336db3c8ce67f
Molly <mojonesycreative@gmail.com>
2022-06-09 13:20:04 -0400

    mapProperties of critic, added update & addCritics functions

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,4 +1,11 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
+
+const addCriticCategory = mapProperties({
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name",
+});
 
 function read(reviewId) {
     return knex("reviews")
@@ -7,9 +14,21 @@ function read(reviewId) {
       .first();
 }
 
+function addCritics(reviewId) {
+    return knex("reviews as r")
+      .join("critics as c", "r.critic_id", "c.critic_id")
+      .select("r.*", "c.*")
+      .where({ "r.review_id": reviewId })
+      .first()
+      .then(addCriticCategory);
+}
+
 function update(updatedReview) {
-    return knex("reviews");
-      
+    return knex("reviews")
+      .select("*")
+      .where({ "review_id": updatedReview.review_id })
+      .update(updatedReview, "*")
+      .then((updatedRecords) => updatedRecords[0]);
 }
 
 function destroy(reviewId) {
@@ -17,6 +36,7 @@ function destroy(reviewId) {
 }
 
 module.exports = {
+    addCritics,
     read,
     update,
     delete: destroy,
